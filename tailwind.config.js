385da6b908eec1c5f2bebcd5188b3cd9bfca1448
22Yash <yash.223234108@vcet.edu.in>
2024-05-13 09:46:06 +0530

    make it for mobile friendly

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // Add other plugins as needed
+  ]
+}
+
